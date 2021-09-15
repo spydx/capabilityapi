240be3afb8f6b21c9ba75c625730cac2dcb2aa0e
Kenneth Fossen <fossen.kenneth@gmail.com>
2021-09-15 12:08:02 +0200

    added a tiny modeltest

--- src/model.rs
+++ src/model.rs
@@ -6,6 +6,7 @@ pub struct User {
     pub name: String,
     pub password: String,
 }
+
 impl fmt::Display for User {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
@@ -13,5 +14,21 @@ impl fmt::Display for User {
             "User => name: {}, password: {}",
             self.name, self.password
         )
+    }
+}
+
+#[cfg(test)]
+mod user_tests {
+    use super::*;
+
+    #[test]
+    fn create_a_user_ok() {
+        let name = "Kenneth".to_string();
+        let password = "password".to_string();
+
+        let u = User { name, password };
+
+        assert_eq!("Kenneth".to_string(), u.name);
+        assert_eq!("password".to_string(), u.password);
     }
 }
